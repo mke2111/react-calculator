4561c9818579c3cde4ec7ccd671f3df85f2c53ac
mke2111 <roymke134@gmail.com>
2021-03-21 02:22:21 +0300

    Add App content

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,7 @@ import '../index.css';
 function App() {
   return (
     <div className="container">
-      <h1>Basic Calculator</h1>
+      <h1>Basic  Calculator</h1>
       <Result />
       <DialPad />
     </div>
