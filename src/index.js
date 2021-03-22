991e55541d739fbb6d5bac5c4ca7dd612aea9889
mke2111 <roymke134@gmail.com>
2021-03-22 23:34:38 +0300

    Refactor App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import './index.css';
 import App from './Components/App';
 
