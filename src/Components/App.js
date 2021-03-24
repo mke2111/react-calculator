7305830690cc1e7ba0f39f1efc6d1905197b0035
mke2111 <roymke134@gmail.com>
2021-03-24 23:43:22 +0300

    Add stateful component in app file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import DialPad from './DialPad';
 import Result from './Result';
 import '../index.css';
 import calculate from '../Logic/Calculate';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Basic  Calculator</h1>
-        <Result value="0" />
-        <DialPad />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+
+  return (
+    <div className="container">
+      <h1>Basic  Calculator</h1>
+      <Result value="0" />
+      <DialPad />
+    </div>
+  );
+};
 
 export default App;
