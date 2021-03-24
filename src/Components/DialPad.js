d7a1641160104c88858ce5114953a60dc66805b6
mke2111 <roymke134@gmail.com>
2021-03-24 23:57:54 +0300

    Update the click handler in dialpd

--- src/Components/DialPad.js
+++ src/Components/DialPad.js
@@ -2,37 +2,37 @@ import React from 'react';
 import '../index.css';
 import SingleButton from './SingleButton';
 
-function DialPad() {
+function DialPad({ clickHandler }) {
   return (
     <div>
       <div className="board-row">
-        <SingleButton name="AC" />
-        <SingleButton name="+" />
-        <SingleButton name="%" />
-        <SingleButton name="/" className="operation" />
+        <SingleButton name="AC" clickHandler={() => clickHandler('AC')} />
+        <SingleButton name="+/-" clickHandler={() => clickHandler('+/-')} />
+        <SingleButton name="%" clickHandler={() => clickHandler('%')} />
+        <SingleButton name="/" clickHandler={() => clickHandler('/')} className="operation" />
       </div>
       <div className="board-row">
-        <SingleButton name="7" />
-        <SingleButton name="8" />
-        <SingleButton name="9" />
-        <SingleButton name="*" className="operation" />
+        <SingleButton name="7" clickHandler={() => clickHandler('7')} />
+        <SingleButton name="8" clickHandler={() => clickHandler('8')} />
+        <SingleButton name="9" clickHandler={() => clickHandler('9')} />
+        <SingleButton name="*" clickHandler={() => clickHandler('*')} className="operation" />
       </div>
       <div className="board-row">
-        <SingleButton name="4" />
-        <SingleButton name="5" />
-        <SingleButton name="6" />
-        <SingleButton name="-" className="operation" />
+        <SingleButton name="4" clickHandler={() => clickHandler('4')} />
+        <SingleButton name="5" clickHandler={() => clickHandler('5')} />
+        <SingleButton name="6" clickHandler={() => clickHandler('6')} />
+        <SingleButton name="-" clickHandler={() => clickHandler('-')} className="operation" />
       </div>
       <div className="board-row">
-        <SingleButton name="1" />
-        <SingleButton name="2" />
-        <SingleButton name="3" />
-        <SingleButton name="+" className="operation" />
+        <SingleButton name="1" clickHandler={() => clickHandler('1')} />
+        <SingleButton name="2" clickHandler={() => clickHandler('2')} />
+        <SingleButton name="3" clickHandler={() => clickHandler('3')} />
+        <SingleButton name="+" clickHandler={() => clickHandler('+')} className="operation" />
       </div>
       <div className="board-row">
         <span><SingleButton name="0" /></span>
-        <SingleButton name="." />
-        <SingleButton name="=" className="operation" />
+        <SingleButton name="." clickHandler={() => clickHandler('.')} />
+        <SingleButton name="=" clickHandler={() => clickHandler('=')} className="operation" />
       </div>
     </div>
   );
