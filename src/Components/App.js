103065bdbe54c30f9e65224470d6360d312e817b
mke2111 <roymke134@gmail.com>
2021-03-25 20:58:32 +0300

    Add class component to the App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,35 +1,59 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import DialPad from './DialPad';
 import Result from './Result';
 import '../index.css';
 import calculate from '../Logic/Calculate';
 
-const App = () => {
-  const [total, setTotal] = useState(null);
-  const [next, setNext] = useState(null);
-  const [operation, setOperation] = useState(null);
+// const App = () => {
+//   const [total, setTotal] = useState(null);
+//   const [next, setNext] = useState(null);
+//   const [operation, setOperation] = useState(null);
 
-  const handleClick = buttonName => {
-    const result = calculate(
-      {
-        total,
-        next,
-        operation,
-      },
-      buttonName,
+//   const handleClick = buttonName => {
+//     const result = calculate(
+//       {
+//         total,
+//         next,
+//         operation,
+//       },
+//       buttonName,
+//     );
+//     setTotal(result.total);
+//     setNext(result.next);
+//     setOperation(result.operation);
+//   };
+
+//   return (
+//     <div className="container">
+//       <h1>Basic  Calculator</h1>
+//       <Result result={total} next={next} />
+//       <DialPad clickHandler={handleClick} />
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = buttonName => this.setState(calculate(this.state, buttonName));
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Basic  Calculator</h1>
+        <Result result={this.state.total || this.state.next} />
+        <DialPad clickHandler={this.handleClick} />
+      </div>
     );
-    setTotal(result.total);
-    setNext(result.next);
-    setOperation(result.operation);
-  };
-
-  return (
-    <div className="container">
-      <h1>Basic  Calculator</h1>
-      <Result result={total} next={next} />
-      <DialPad clickHandler={handleClick} />
-    </div>
-  );
-};
+  }
+}
 
 export default App;
