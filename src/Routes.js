588d2ba627cf9147fc9274eff46fd6bf157c1358
mke2111 <roymke134@gmail.com>
2021-03-30 00:05:24 +0300

    Fix all eslint errors

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import App from "./App";
-import Home from "./home";
-import Quote from "./quote";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './App';
+import Home from './home';
+import Quote from './quote';
 
-const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/app" component={App} />
-        <Route exact path="/quote" component={Quote} />
-        <Route exact path="/home" component={Home} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/app" component={App} />
+      <Route exact path="/quote" component={Quote} />
+      <Route exact path="/home" component={Home} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Routes;
--- src/quote.js
+++ src/quote.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Quote = () => <h1>Quote of the day</h1>;
+const Quote = () => (
+  <div>
+    <h1>Quote of the day</h1>
+    <p>
+      This simple calculator is made with react. The project ensures proper user of routing in react
+    </p>
+  </div>
+);
 
 export default Quote;
