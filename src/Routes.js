946be4a81631a197a48706ec39052a7050bec623
mke2111 <roymke134@gmail.com>
2021-03-29 22:50:09 +0300

    add rotes file

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/profile" component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
