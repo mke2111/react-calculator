0603f168126b029df7d8afee07604cd828061476
mke2111 <roymke134@gmail.com>
2021-03-29 23:06:45 +0300

    add quote file

---
+++ src/quote.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Quote = () => {
+  return (
+    <>
+      <h1>Quote of the day</h1>
+      <p>
+      	This is a simple calculator made with react. This project ensures proper user of routing in react
+      </p>
+    </>
+  );
+};
+
+export default Quote;
