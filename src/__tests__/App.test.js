aaf7e215991809aca2cf85a93199d32f3270e5c1
mke2111 <roymke134@gmail.com>
2021-03-30 05:11:08 +0300

    Add tests for App and quote files

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('Calculator snapshot', () => {
@@ -9,5 +11,59 @@ describe('Calculator snapshot', () => {
       .create(<App />)
       .toJSON();
     expect(calculator).toMatchSnapshot();
+  });
+});
+
+describe('Calculator methods', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('it test add numbers', async () => {
+    userEvent.click(screen.getByText('6'));
+    userEvent.click(screen.getByText('+'));
+    userEvent.click(screen.getByText('6'));
+    userEvent.click(screen.getByText('='));
+    const display = await screen.findByText('12');
+    expect(display).toBeInTheDocument();
+  });
+
+  it('test to subtract numbers', async () => {
+    userEvent.click(screen.getByText('6'));
+    userEvent.click(screen.getByText('8'));
+    userEvent.click(screen.getByText('-'));
+    userEvent.click(screen.getByText('6'));
+    userEvent.click(screen.getByText('='));
+    const display = await screen.findByText('62');
+    expect(display).toBeInTheDocument();
+  });
+
+  it('test to multiply numbers', async () => {
+    userEvent.click(screen.getByText('6'));
+    userEvent.click(screen.getByText('8'));
+    userEvent.click(screen.getByText('*'));
+    userEvent.click(screen.getByText('2'));
+    userEvent.click(screen.getByText('='));
+    const display = await screen.findByText('136');
+    expect(display).toBeInTheDocument();
+  });
+
+  it('test to divide numbers', async () => {
+    userEvent.click(screen.getByText('6'));
+    userEvent.click(screen.getByText('8'));
+    userEvent.click(screen.getByText('/'));
+    userEvent.click(screen.getByText('2'));
+    userEvent.click(screen.getByText('='));
+    const display = await screen.findByText('34');
+    expect(display).toBeInTheDocument();
+  });
+
+  it('test for decimal numbers', async () => {
+    userEvent.click(screen.getByText('6'));
+    userEvent.click(screen.getByText('.'));
+    userEvent.click(screen.getByText('2'));
+    userEvent.click(screen.getByText('='));
+    const display = await screen.findByText('6.2');
+    expect(display).toBeInTheDocument();
   });
 });
